04cf3f5fcf2d92a19b9469c4ae140415c9d679a3
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-12 14:14:01 +0530

    added all request DTOs

---
+++ Application/LibrarySystemAPI.Application/DTOs/Requests/BorrowBookRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystemAPI.Application.DTOs
+{
+    public class BorrowBookRequest
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int PublicationYear { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Member ID must be a positive integer.")]
+        public int MemberID { get; set; }
+    }
+}
---
+++ Application/LibrarySystemAPI.Application/DTOs/Requests/CreateMemberRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystemAPI.Application.DTOs
+{
+    public class CreateMemberRequest
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 100 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0,2)]
+        public int MemberType { get; set; } // 0 for Member, 1 for Minor Staff, 2 for Management Staff
+    }
+}
