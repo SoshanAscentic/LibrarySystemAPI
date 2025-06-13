a568cf5e6f9e447755ac4d4424dfe024301913fa
Soshan Wijayarathne <soshan.wijayarathne@ascentic.se>
2025-06-13 09:52:19 +0530

    added logic to AuthController

--- API/LibrarySystemAPI.API/Controllers/AuthController.cs
+++ API/LibrarySystemAPI.API/Controllers/AuthController.cs
@@ -1,6 +1,70 @@
-﻿namespace LibrarySystemAPI.API.Controllers
+﻿using LibrarySystemAPI.Application.DTOs;
+using LibrarySystemAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystemAPI.API.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route ("api/[controller]")]
+    [Produces ("application/json")]
+    public class AuthController :ControllerBase
     {
+        private readonly IAuthenticationService authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            this.authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
+        }
+
+
+        [HttpPost("login")]
+        [ProducesResponseType(typeof(MemberDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<MemberDto> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var member = authenticationService.Login(loginDto);
+                if (member == null)
+                    return NotFound("Member not found. please sign up first");
+                return Ok(member);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occured during login");
+            }
+        }
+
+
+        [HttpPost("signup")]
+        [ProducesResponseType(typeof(MemberDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<MemberDto> Signup(CreateMemberDto createMemberDto)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var member = authenticationService.SignUp(createMemberDto);
+                return CreatedAtAction(nameof(Login), new { memberId = member.MemberID }, member);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred during signup.");
+            }
+        }
+
     }
 }
